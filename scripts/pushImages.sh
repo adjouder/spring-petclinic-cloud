23d0ebae93d5c0275c99020583dcbb3712774ce6
DJOUDER <djouder@Ordinateur-portable-de-DJOUDER.local>
2024-05-10 20:30:37 +0200

    split des pipelines

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-REPOSITORY_PREFIX=adjouder
-echo "début du déploeiement ......."
+echo "début du déploiement ......."
 echo ${REPOSITORY_PREFIX}"/spring-petclinic-cloud-api-gateway:latest"
 
+#!/bin/bash
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
-
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
+#docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
